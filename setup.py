372a65b4af410ce4bb6e2de6b44c803f93a9bd35
Muthupandian <muthupandiansuresh2003@gmail.com>
2026-01-31 19:44:57 +0530

    setup file created

--- setup.py
+++ setup.py
@@ -2,4 +2,17 @@ from setuptools import setup
 from typing import List
 
 def get_requirements(filepath:str)-> List[str]:
-    with open(filepath: str = 'requiremnts.txt' 'r')  as file:
+    with open(filepath,'r')  as file:
+        requirements=file.readlines()
+        print(requirements)
+        req=[lib.strip() for lib in requirements if lib.strip() != '-e .']
+        return req
+    
+setup(
+    name='---',
+    version='1.1.1',
+    author='Muthupandian',
+    author_email='muthupandiansuresh2003@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
